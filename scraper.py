63c5a5e491cfa36d2d71b9ebc3e41920b97943d7
nilberthsouza <nilberth@protonmail.com>
2019-11-26 18:59:58 -0300

    feat: app que checa site do mercado livre para ver se o preço de um produto caiu. Parcialmente funcionando , ainda preciso terminar

---
+++ scraper.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+page = requests.get('https://produto.mercadolivre.com.br/MLB-698976428-peruca-cosplay-branca-prata-prontaentrega-_JM')
+
+soup = BeautifulSoup(page.text, 'html.parser')
+title = soup.find(class_='item-title__primary')
+print(title.string)
+
+soup = BeautifulSoup(page.text,'html.parser')
+price = soup.find(class_='price-tag-fraction')
+price_conv =int(price.string)
+print(price_conv)
+
+if price_conv < 100:
+    print("preço abaixo")
+else:
+    pass
+
+
+def price_checker():
+    if price_conv < 100:
+        email_sender()
+    else:
+        pass
+
+
+while(true):
+    price_checker()
+        time.sleep(100)
