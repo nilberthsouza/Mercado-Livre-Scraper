e4f5d750ccea5aa7fbadbe78057f0ab11c00d2b9
nilberthsouza <nilberth@protonmail.com>
2019-11-26 21:55:11 -0300

    fix issue #1

--- scraper.py
+++ scraper.py
@@ -5,14 +5,31 @@ import smtplib
 
 page = requests.get('https://produto.mercadolivre.com.br/MLB-698976428-peruca-cosplay-branca-prata-prontaentrega-_JM')
 
-soup = BeautifulSoup(page.text, 'html.parser')
-title = soup.find(class_='item-title__primary')
-print(title.string)
+def get_info(in_value):
+    soup = BeautifulSoup(page.text, 'html.parser')
+    out_value = soup.find(class_=in_value)
+    return out_value.string
 
-soup = BeautifulSoup(page.text,'html.parser')
-price = soup.find(class_='price-tag-fraction')
-price_conv =int(price.string)
-print(price_conv)
+def convert_value_to_num(value_str):
+    out_value_flt  = float(value_str)
+    return out_value_flt
+
+
+def price_checker(price_conv):
+    if price_conv < 100:
+        email_sender()
+    else:
+        print("sistema funcionando")
+        pass
+
+while(True):
+    get_info('item-title__primary')
+    price_conv =convert_value_to_num(get_info('price-tag-fraction'))
+    price_checker(price_conv)
+    time.sleep(100)
+
+
+
 
 def email_sender():
     mail_from = 'Home@nilberth.com'
@@ -32,14 +49,14 @@ def email_sender():
     server.sendmail(mail_from, mail_to, mail_subject, mail_message)
     server.quit()
 
-def price_checker():
-    if price_conv < 100:
-        email_sender()
-    else:
-        print("sistema funcionando")
-        pass
 
+'''
+soup = BeautifulSoup(page.text, 'html.parser')
+title = soup.find(class_='item-title__primary')
+print(title.string)
 
-while(True):
-    price_checker()
-    time.sleep(100)
+soup = BeautifulSoup(page.text,'html.parser')
+price = soup.find(class_='price-tag-fraction')
+price_conv =int(price.string)
+print(price_conv)
+'''
